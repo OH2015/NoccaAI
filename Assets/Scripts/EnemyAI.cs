78fb80383780b58598a6d92ba2463d4d36e85543
OH2015 <fibo2955@gmail.com>
2024-04-10 14:59:09 +0900

    first commit

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EnemyAI : MonoBehaviour
+{
+  private int MAX_DEPTH = 5;
+
+  void putMap(int[,] map,int[] node,int turn)
+  {
+    if (map[node[0], 2] != 0) {
+      map[node[0],2] = 0;
+    }else if (map[node[0], 1] != 0){
+      map[node[0],1] = 0;
+    } else if(map[node[0],0] != 0){
+      map[node[0],0] = 0;
+    }
+    if(map[node[1],1]!=0){
+      map[node[1],2] = turn;
+    }else if(map[node[1],0] != 0){
+      map[node[1],1] = turn;
+    }else{
+      map[node[1],0] = turn;
+    }
+  }
+
+  int[,] getNodeList_advanced(int[,] map,int turn)
+  {
+      List<int> nd1list = new List<int>();
+      List<int> nd2list = new List<int>();
+      int maxValue = 0;
+      for(int h = 0; h < 40; h++)
+      {
+        // 自分のゴールをスキップ
+        if (h < 5) continue;
+        if(map[h,2] == turn || (map[h,2] == 0 && map[h,1] == turn) || (map[h,1] == 0 && map[h,0] == turn)){
+          for (int i = -1; i <= 1; i++)
+          {
+            for (int j = -1; j <= 1; j++)
+            {
+              // 枠外と現在のマスをスキップ
+              if ((h < 5 && i == -1) || (24 < h && i == 1)) continue;
+              if ((h % 5 == 0 && j == -1) || (h % 5 == 4 && j == 1)) continue;
+              if (i == 0 && j == 0) continue;
+
+              int p = h + 5 * i + j;
+              if (map[p, 2] == 0)
+              {
+                  nd1list.Add(h);
+                  nd2list.Add(p);
+              }
+            }
+          }
+        }
+      }
+      int numberOfNodes = nd1list.Count;
+      int[,] nodeList = new int[numberOfNodes,2];
+
+      int[] valueList_sort = new int[numberOfNodes];
+      int[] valueList_org = new int[numberOfNodes];
+
+      for(int i = 0;i<numberOfNodes;i++){
+        nodeList[i,0] = nd1list[i];
+        nodeList[i,1] = nd2list[i];
+        int[] nd = {nd1list[i],nd2list[i]};
+
+        putMap(map,nd,turn);
+
+        int value = evalMap(map);
+        valueList_org[i] = value;
+        valueList_sort[i] = value;
+        Array.Reverse(nd);
+
+        putMap(map,nd,turn);
+      }
+
+      Array.Sort(valueList_sort);
+      if(turn == 1){
+        Array.Reverse(valueList_sort);
+      }
+      int[,] nodeList_sorted = new int[numberOfNodes,2];
+      for(int i = 0;i<numberOfNodes;i++){
+        int index = Array.IndexOf(valueList_org ,valueList_sort[i]);
+        nodeList_sorted[i,0] = nodeList[index,0];
+        nodeList_sorted[i,1] = nodeList[index,1];
+
+        // 同じ評価値が続いたときにハンドが重複しないようにするため
+        // 例えばvalueList_sort = [6,6,5,3,2,1]
+        // valueLis_org = [3,2,5,6,1,6] ここで評価値６のハンドを代入した後
+        // -> valueList_org = [3,2,5,7,1,6] という風に値を変えると同じハンドを代入してしまうことがなくなる。
+        if(turn == 1){
+          valueList_org[index] += 1;
+        }else{
+          valueList_org[index] -= 1;
+        }
+      }
+
+      return nodeList_sorted;
+  }
+
+  int[,] getNodeList(int[,] map,int turn)
+  {
+      List<int> nd1list = new List<int>();
+      List<int> nd2list = new List<int>();
+      int maxValue = 0;
+      for(int h = 0; h < 30; h++)
+      {
+        // 自分のゴールをスキップ
+        if (h < 5) continue;
+        if(map[h,2] == turn || (map[h,2] == 0 && map[h,1] == turn) || (map[h,1] == 0 && map[h,0] == turn)){
+          for (int i = -1; i <= 1; i++)
+          {
+            for (int j = -1; j <= 1; j++)
+            {
+              // 枠外と現在のマスをスキップ
+              if ((h < 5 && i == -1) || (24 < h && i == 1)) continue;
+              if ((h % 5 == 0 && j == -1) || (h % 5 == 4 && j == 1)) continue;
+              if (i == 0 && j == 0) continue;
+
+              int p = h + 5 * i + j;
+              if (map[p, 2] == 0)
+              {
+                  nd1list.Add(h);
+                  nd2list.Add(p);
+              }
+            }
+          }
+        }
+      }
+      int numberOfNodes = nd1list.Count;
+      int[,] nodeList = new int[numberOfNodes,2];
+      for(int i = 0;i<numberOfNodes;i++){
+        nodeList[i,0] = nd1list[i];
+        nodeList[i,1] = nd2list[i];
+      }
+
+      return nodeList;
+  }
+
+  int[,] getNodeList_forward(int[,] map,int turn)
+  {
+    List<int> nd1list = new List<int>();
+    List<int> nd2list = new List<int>();
+    int maxValue = 0;
+    for(int h = 0; h < 30; h++)
+    {
+      if(map[h,2] == turn || (map[h,2] == 0 && map[h,1] == turn) || (map[h,1] == 0 && map[h,0] == turn)){
+        for (int i = -1; i <= 1; i++)
+        {
+          for (int j = -1; j <= 1; j++)
+          {
+            // 枠外と現在のマスをスキップ
+            if ((h < 5 && i == -1) || (24 < h && i == 1)) continue;
+            if ((h % 5 == 0 && j == -1) || (h % 5 == 4 && j == 1)) continue;
+            if (i == 0 && j == 0) continue;
+
+            int p = h + 5 * i + j;
+            if (map[p, 2] == 0)
+            {
+                nd1list.Add(h);
+                nd2list.Add(p);
+            }
+          }
+        }
+      }
+    }
+    int numberOfNodes = nd1list.Count;
+    int[,] nodeList = new int[numberOfNodes,2];
+
+    int[] valueList_sort = new int[numberOfNodes];
+    int[] valueList_org = new int[numberOfNodes];
+
+    for(int i = 0;i<numberOfNodes;i++){
+      nodeList[i,0] = nd1list[i];
+      nodeList[i,1] = nd2list[i];
+
+      valueList_org[i] = nd2list[i];
+      valueList_sort[i] = nd2list[i];
+    }
+
+    Array.Sort(valueList_sort);
+    if(turn == -1){
+      Array.Reverse(valueList_sort);
+    }
+    int[,] nodeList_sorted = new int[numberOfNodes,2];
+    for(int i = 0;i<numberOfNodes;i++){
+      int index = Array.IndexOf(valueList_org ,valueList_sort[i]);
+      nodeList_sorted[i,0] = nodeList[index,0];
+      nodeList_sorted[i,1] = nodeList[index,1];
+
+      // 同じ評価値が続いたときにハンドが重複しないようにするため
+      // 例えばvalueList_sort = [6,6,5,3,2,1]
+      // valueLis_org = [3,2,5,6,1,6] ここで評価値６のハンドを代入した後
+      // -> valueList_org = [3,2,5,7,1,6] という風に値を変えると同じハンドを代入してしまうことがなくなる。
+      if(turn == 1){
+        valueList_org[index] -= 1;
+      }else{
+        valueList_org[index] += 1;
+      }
+    }
+
+    return nodeList_sorted;
+  }
+
+  int evalMap(int[,] map)
+  {
+    int ev = 0;
+
+    for(int i = 0; i < 30; i++)
+    {
+
+        if(map[i,0] == 0)continue;
+
+        // 相手の駒二つの上に乗るとかなり有利
+        if(map[i,2] == 1 && map[i,1] == -1 && map[i,0] == -1)
+        {
+            ev += 5;
+        }
+        else if(map[i,2] == -1 && map[i,1] == 1 && map[i,0] == 1)
+        {
+            ev -= 5;
+        }
+
+        // 相手の上に乗っているとそこそこ有利
+        if(map[i,0] == 1 && map[i,1] == -1){
+          ev -= 3;
+        }
+        else if(map[i,0] == -1 && map[i,1] == 1){
+          ev += 3;
+        }
+
+        // 駒が前にあるほど有利
+        for(int f = 0;f < 3;f++){
+          if(map[i,f] == 1){
+            if(i/5 == 1)ev += 2;
+            if(i/5 == 2)ev += 4;
+            ev += 5 - i/5;
+          }else if(map[i,f] == -1){
+            if(i/5 == 4)ev -= 2;
+            if(i/5 == 5)ev -= 4;
+            ev -= i/5;
+          }
+        }
+
+
+    }
+    return ev;
+
+  }
+
+  public (int[],int) thinkHand(int[,] map,int moveNumber)
+  {
+    int depth = 1;
+    if(moveNumber == 0){
+      depth = 1;
+    }else if(moveNumber < MAX_DEPTH){
+      depth = moveNumber;
+    }else{
+      depth = MAX_DEPTH;
+    }
+    var sw = new System.Diagnostics.Stopwatch();
+    sw.Start();
+
+    var(hand, sc) = deepThink_ab(map, -1, depth, 9999 * -1, 9999);
+    sw.Stop();
+    TimeSpan ts = sw.Elapsed;
+    Debug.Log("思考時間: "+ts.Minutes+"分" + ts.Seconds + "秒");
+    int ev = evalMap(map);
+    Debug.Log("ev: " + ev);
+    return (hand,sc);
+  }
+
+  (int[],int)deepThink_ab(int[,] map,int turn,int depth,int a,int b)
+  {
+    int[] besthand = null;
+
+    for (int i = 0;i<40;i++){
+      if (i < 6 && turn == 1)
+      {
+          if (map[i, 2] == 1 || (map[i, 2] == 0 && map[i, 1] == 1) || (map[i, 2] == 0 && map[i, 1] == 0 && map[i, 0] == 1))
+          {
+              return(null,999);
+          }
+      }
+      else if (29 < i && turn == -1)
+      {
+          if (map[i, 2] == -1 || (map[i, 2] == 0 && map[i, 1] == -1) || (map[i, 2] == 0 && map[i, 1] == 0 && map[i, 0] == -1))
+          {
+            return(null,-999);
+          }
+      }
+    }
+
+    if(depth == 0)
+    {
+        return (null, evalMap(map));
+    }
+
+    int[,] nodeList = getNodeList(map, turn);
+    if(depth > 3 && depth <9){
+      nodeList = getNodeList_advanced(map,turn);
+    }
+
+    for(int i = 0;(a < b) && (i < nodeList.GetLength(0)); i++)
+    {
+        int[] hand = {nodeList[i,0],nodeList[i,1]};
+        int[] hand_reverse = {nodeList[i,1],nodeList[i,0]};
+
+        putMap(map, hand,turn);
+
+
+        var (bHand,sc) = deepThink_ab(map, turn * -1, depth - 1, a, b);
+
+        // ここでマップを戻す
+        putMap(map,hand_reverse,turn);
+
+        // if(besthand == null)
+        // {
+        //     besthand = hand;
+        //     if(turn == 1)a = sc;
+        //     else b = sc;
+        // }
+
+        if (turn == 1 && sc > a)
+        {
+            a = sc;
+            besthand = hand;
+        }
+        else if (turn == -1 && sc < b)
+        {
+            b = sc;
+            besthand = hand;
+        }
+    }
+    int bestscore = 0;
+    if(turn == 1)bestscore = a;
+    if(turn == -1)bestscore = b;
+    return (besthand, bestscore);
+  }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO.Pipes;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+  public Material selectingMat;
+  public Material whitemat;
+  public Material pinkmat;
+  public GameObject enemy;
+  public GameObject noccaPrefab;
+  public GameObject enemyNoccaPrefab;
+  public GameObject messageText;
+  private GameObject selecting = null;
+  private int moved = 0;
+  private Boolean isEnemyThinking = false;
+  private int winner = 0;
+
+  int[,] _map = {
+      {0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},
+      {-1,0,0},{-1,0,0},{-1,0,0},{-1,0,0},{-1,0,0},
+      {0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},
+      {0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},
+      {0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},
+      {0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},
+      {1,0,0},{1,0,0},{1,0,0},{1,0,0},{1,0,0},
+      {0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},
+    };
+
+  // Start is called before the first frame update
+  void Update()
+  {
+    if (winner != 0){
+      Text text = messageText.GetComponent<Text>();
+      text.text = (winner > 0 ? "You " : "Enemy ") + "Win!";
+    }
+    else if (isEnemyThinking)
+    {
+      Text text = messageText.GetComponent<Text>();
+      text.text = "Thinking...";
+      EnemyAction();
+      text.text = "";
+      isEnemyThinking = false;
+    }
+    else if (Input.GetMouseButtonDown(0))
+    {
+      HandleMouseClick();
+    }
+  }
+
+  private void HandleMouseClick()
+  {
+    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    RaycastHit hit;
+    if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+    {
+      if (hit.collider.gameObject.CompareTag("Squares"))
+      {
+        if (selecting == null)
+        {
+          selecting = hit.collider.gameObject;
+          selecting.GetComponent<Renderer>().material = selectingMat;
+        }
+        else
+        {
+          int[] nd = { int.Parse(selecting.name), int.Parse(hit.collider.gameObject.name) };
+          if (int.Parse(selecting.name) % 2 == 0) selecting.GetComponent<Renderer>().material = whitemat;
+          else selecting.GetComponent<Renderer>().material = pinkmat;
+          selecting = null;
+          Action(nd);
+        }
+      }
+    }
+  }
+
+  void MoveNocca(int[] node, int turn)
+  {
+    if (node[0] == 0 && node[1] == 0) return;
+    int height1 = 0;
+    int height2 = 0;
+    if (_map[node[0], 2] != 0)
+    {
+      height1 = 3;
+      _map[node[0], 2] = 0;
+    }
+    else if (_map[node[0], 1] != 0)
+    {
+      height1 = 2;
+      _map[node[0], 1] = 0;
+    }
+    else if (_map[node[0], 0] != 0)
+    {
+      height1 = 1;
+      _map[node[0], 0] = 0;
+    }
+
+
+    if (_map[node[1], 1] != 0)
+    {
+      height2 = 2;
+      _map[node[1], 2] = turn;
+    }
+    else if (_map[node[1], 0] != 0)
+    {
+      height2 = 1;
+      _map[node[1], 1] = turn;
+    }
+    else
+    {
+      _map[node[1], 0] = turn;
+    }
+    SyncPosition();
+    // ゴールしてたらゲーム終了
+    if(turn == 1 && node[1] < 5){
+      winner = 1;
+    }else if(turn == -1 && node[1] > 34){
+      winner = -1;
+    }
+  }
+
+
+  public void Reset()
+  {
+    _map = new int[,]{
+      {0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},
+      {-1,0,0},{-1,0,0},{-1,0,0},{-1,0,0},{-1,0,0},
+      {0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},
+      {0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},
+      {0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},
+      {0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},
+      {1,0,0},{1,0,0},{1,0,0},{1,0,0},{1,0,0},
+      {0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},
+    };
+    moved = 0;
+    winner = 0;
+    SyncPosition();
+    Text text = messageText.GetComponent<Text>();
+    text.text = "";
+  }
+
+  private void SyncPosition()
+  {
+    int num = 1, num_e = 1;
+    // mapを検索して駒の位置を調整
+    for (int i = 0; i < _map.GetLength(0); i++)
+    {
+      for (int j = 0; j < _map.GetLength(1); j++)
+      {
+        if(_map[i, j] != 0){
+          var nc = (_map[i, j] == 1) ? GameObject.Find("Nocca" + num++) : GameObject.Find("Nocca_E" + num_e++);
+          nc.transform.position = new Vector3(-i % 5, 0.8f + j*0.8f, (i/5));
+        }
+      }
+    }
+  }
+
+
+  public void Pass()
+  {
+    var (enemyhand, enemyScore) = enemy.GetComponent<EnemyAI>().thinkHand(_map, moved);
+    MoveNocca(enemyhand, -1);
+  }
+
+  void Action(int[] nd)
+  {
+    if (!CanPut(_map, nd, 1)) return;
+    moved += 1;
+    MoveNocca(nd, 1);
+    isEnemyThinking = true;
+  }
+
+  void EnemyAction()
+  {
+    var (enemyhand, enemyScore) = enemy.GetComponent<EnemyAI>().thinkHand(_map, moved);
+    UnityEngine.Debug.Log(moved);
+    UnityEngine.Debug.Log("hand: " + enemyhand[0]+enemyhand[1] + ", sc: " + enemyScore);
+    MoveNocca(enemyhand, -1);
+  }
+
+  Boolean CanPut(int[,] map, int[] node, int turn)
+  {
+    int p1 = node[0];
+    int p2 = node[1];
+    // 移動元：自分の駒が上にあるか判定
+    if (map[p1, 2] == -turn) return false;
+    if (map[p1, 2] == 0 && map[p1, 1] == -turn) return false;
+    if (map[p1, 0] != turn && map[p1, 1] != turn && map[p1, 2] != turn) return false;
+    // 移動先：周囲8マスにあるかどうか判定
+    int row = p1 / 5;
+    int column = p1 % 5;
+    int row_to = p2 / 5;
+    int column_to = p2 % 5;
+    if (row_to < row - 1 || row + 1 < row_to) return false;
+    if (column_to < column - 1 || column + 1 < column_to) return false;
+    if (p1 == p2) return false;
+    // ３個乗っていないか判定
+    if (map[p2, 2] != 0) return false;
+    // 自分の陣地かどうか判定
+    if ((turn == 1 && p2 > 34) || (turn == -1 && p2 < 5)) return false;
+    return true;
+  }
+
+}
